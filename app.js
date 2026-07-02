d8f97a47c453f3a68246b1aed4e59837438f475f
mabdelkaderppbuild <mabdelkader@ppbuil2024.onmicrosoft.com>
2026-07-01 20:39:09 -0700

    Initial update

---
+++ app.js
@@ -0,0 +1,11 @@
+// Simple Node.js application
+console.log('Welcome to the test application!');
+
+// Main application logic
+function main() {
+  console.log('Application started...');
+  console.log('Ready to run!');
+}
+
+// Run the application
+main();
